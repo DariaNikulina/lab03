03a14fb10d318a1bbb19f9669bee0d983099ddc4
DariaNikulina <110302997+DariaNikulina@users.noreply.github.com>
2022-11-29 04:26:31 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-print("Введите вариант сортировки (по возрастанию / по убыванию):")
-s = input()
-
 n = int(input("Введите количество чисел: "))
 print("Введите числа:")
 
@@ -11,20 +8,13 @@ for i in range(n):
 
 print("Исходный массив:")
 print(mas)
-if (s == "по возрастанию"):
-    for i in range(n - 1):
-        for j in range(n - i - 1):
-            if mas[j] > mas[j + 1]:
-                tmp = mas[j]
-                mas[j] = mas[j + 1]
-                mas[j + 1] = tmp
-if (s == "по убыванию"):
-    for i in range(n - 1):
-        for j in range(n - i - 1):
-            if mas[j] < mas[j + 1]:
-                tmp = mas[j]
-                mas[j] = mas[j + 1]
-                mas[j + 1] = tmp
+
+for i in range(n - 1):
+    for j in range(n - i - 1):
+        if mas[j] > mas[j + 1]:
+            tmp = mas[j]
+            mas[j] = mas[j + 1]
+            mas[j + 1] = tmp
 
 print("Массив после сортировки пузырьком:")
 print(mas)
